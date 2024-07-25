0f7003957021a24a897adb118b14006fd7f73b5d
Andrew Worley <157335202+Worley03@users.noreply.github.com>
2024-07-25 10:17:16 -0500

    Update app.js
    Same as last, 15 instead of 3

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ const io = new socketIo.Server(httpServer, {
     },
     connectionStateRecovery: {
         // the backup duration of the sessions and the packets
-        maxDisconnectionDuration: 3 * 1000,
+        maxDisconnectionDuration: 10 * 1000,
         // whether to skip middlewares upon successful recovery
         skipMiddlewares: true,
       }
